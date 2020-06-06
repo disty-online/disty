d6cb4152f55743d4d406666bbdfd3a2830e945ab
Lala <flasanches@gmail.com>
2020-06-06 17:58:33 -0400

    Add User model

--- disty/models.py
+++ disty/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Role(models.Model):
     role_name = models.CharField(max_length=50)
 
-# class User(models.Model):
-#     role = models.ForeignKey(Role, on_delete=models.CASCADE)
-#     name = models.CharField(max_length=100)
-#     password = models.CharField(max_length=100)
+class User(models.Model):
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, db_index=True)
+    password = models.CharField(max_length=100)
