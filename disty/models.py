9f721db3b969c2426f7255c5baf71177f4f06769
Eri Bastos <bastos.eri@gmail.com>
2020-06-06 19:17:41 -0400

    Add File and Url Models with Tests

--- disty/models.py
+++ disty/models.py
@@ -15,3 +15,23 @@ class User(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class File(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    checksum = models.CharField(max_length=150)
+    storage_location = models.CharField(max_length=100)
+    path = models.CharField(max_length=256)
+    origin = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    created_at = models.DateTimeField("created at")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Url(models.Model):
+    expiry = models.DateTimeField("created at")
+    download_count = models.IntegerField(default=0)
+    url = models.CharField(max_length=250)
+    created_at = models.DateTimeField("created at")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    file = models.ForeignKey(File, on_delete=models.CASCADE)
--- disty/tests.py
+++ disty/tests.py
@@ -1,14 +1,37 @@
+import datetime
 from django.test import TestCase
-from disty.models import Role, User
+from django.utils import timezone
+import pytz
+from disty.models import Role, User, File, Url
 
 # Create your tests here.
 
 
 class ModelsTestCase(TestCase):
     def setUp(self):
+        tomorrow = timezone.now() + datetime.timedelta(days=1)
         Role.objects.create(role_name="admin")
         User.objects.create(
             name="admin_user", password="password", role=Role.objects.get(pk=1)
+        )
+        File.objects.create(
+            name="myfile.txt",
+            checksum="sjhskjdhskjdhksajd",
+            storage_location="local",
+            path="/local/some/path",
+            origin="internal",
+            password="my_password",
+            created_at=timezone.now(),
+            owner=User.objects.get(pk=1),
+        )
+
+        Url.objects.create(
+            expiry=tomorrow,
+            download_count=0,
+            url="http://some/link/url",
+            created_at=timezone.now(),
+            owner=User.objects.get(pk=1),
+            file=File.objects.get(pk=1),
         )
 
     def test_create(self):
