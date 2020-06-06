d7f1d4b5ae20db838587dc6b15cd83cdd27c4a43
Eri Bastos <bastos.eri@gmail.com>
2020-06-06 18:54:25 -0400

    Add initial tests

--- disty/models.py
+++ disty/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
+
 class Role(models.Model):
     role_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.role_name
+
 
 class User(models.Model):
     role = models.ForeignKey(Role, on_delete=models.CASCADE)
     name = models.CharField(max_length=100, db_index=True)
     password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
--- disty/tests.py
+++ disty/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from disty.models import Role, User
 
 # Create your tests here.
+
+
+class ModelsTestCase(TestCase):
+    def setUp(self):
+        Role.objects.create(role_name="admin")
+        User.objects.create(
+            name="admin_user", password="password", role=Role.objects.get(pk=1)
+        )
+
+    def test_create(self):
+        admin = Role.objects.get(role_name="admin")
+        assert admin.role_name == "admin"
+
+    def test_create(self):
+        admin = User.objects.get(name="admin_user")
+        assert admin.role_id == 1
