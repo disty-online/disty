80c7d3a1bd29fc2f29038237fe36838300c0ff5b
Eri Bastos <eri.bastos@molex.com>
2020-06-08 17:50:48 -0400

    Customize models display on admin site

--- disty/admin.py
+++ disty/admin.py
@@ -3,8 +3,21 @@ from django.contrib import admin
 # Register your models here.
 from .models import Role, User, File, Url, Access
 
+
+class FileAdmin(admin.ModelAdmin):
+    list_display = ("name", "owner", "created_at", "checksum")
+
+
+class AccessAdmin(admin.ModelAdmin):
+    list_display = ("url", "file", "timestamp", "source_ip", "user_agent")
+
+
+class UrlAdmin(admin.ModelAdmin):
+    list_display = ("url", "owner", "file", "created_at", "expiry")
+
+
 admin.site.register(Role)
 admin.site.register(User)
-admin.site.register(File)
-admin.site.register(Url)
-admin.site.register(Access)
+admin.site.register(File, FileAdmin)
+admin.site.register(Url, UrlAdmin)
+admin.site.register(Access, AccessAdmin)
