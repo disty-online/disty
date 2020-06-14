7d8084a0ebd3ca9a860822738dfd885ae56fd5d0
Flavia Bastos <flasanches@gmail.com>
2020-06-14 09:52:23 -0400

    Allow user to update their password

--- disty/urls.py
+++ disty/urls.py
@@ -4,6 +4,12 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from disty import views
+from django.contrib.auth.views import (
+    # LogoutView,
+    PasswordChangeView,
+    PasswordChangeDoneView,
+)
+
 
 urlpatterns = [
     url(r"^$", views.home, name="home"),
@@ -13,4 +19,5 @@ urlpatterns = [
     url(r"^uploads/form/$", views.model_form_upload, name="model_form_upload"),
     url(r"^upload/(?P<ruuid>.*)$", views.upload, name="upload"),
     url(r"^new_url/", views.new_url, name="new_url"),
+    path("password_change/", PasswordChangeView.as_view(success_url="/disty")),
 ]
