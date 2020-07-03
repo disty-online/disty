18416f80f694119a264b8e1998e6acb475fa75bf
Eri Bastos <bastos.eri@gmail.com>
2020-07-03 09:59:40 -0400

    Fix spacing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "[$(date -u "+%Y-%m-%d  %H:%M:%S +0000")] [INFO] Starting disty"
-echo "[$(date -u "+%Y-%m-%d  %H:%M:%S +0000")] Creating superuser"
+echo "[$(date -u "+%Y-%m-%d %H:%M:%S +0000")] [INFO] Starting disty"
+echo "[$(date -u "+%Y-%m-%d %H:%M:%S +0000")] Creating superuser"
 python manage.py createsuperuser --noinput
 
-echo "[$(date -u "+%Y-%m-%d  %H:%M:%S +0000")] Starting Gunicorn"
+echo "[$(date -u "+%Y-%m-%d %H:%M:%S +0000")] Starting Gunicorn"
 gunicorn --bind 0.0.0.0:8000 -w 4 disty_online.wsgi --preload
