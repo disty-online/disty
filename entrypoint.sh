5219b880541fdbbae557f3e926157ff694dcb706
Eri Bastos <bastos.eri@gmail.com>
2020-07-04 10:48:20 -0400

    Add logging to gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,4 +11,5 @@ echo "[$(date -u "+%Y-%m-%d %H:%M:%S +0000")] Creating superuser"
 python manage.py createsuperuser --noinput && :
 
 echo "[$(date -u "+%Y-%m-%d %H:%M:%S +0000")] Starting Gunicorn"
-gunicorn --bind 0.0.0.0:8000 -w 4 disty_online.wsgi --preload
+gunicorn --bind 0.0.0.0:8000 -w 4 disty_online.wsgi --preload --access-logfile - --error-logfile -
+
